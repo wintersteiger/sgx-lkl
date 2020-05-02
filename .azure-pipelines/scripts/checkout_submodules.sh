d0ae459a5b5089bf91e2e45e7a8d21a65ec6f9e1
Maik Riechert <marieche@microsoft.com>
2020-05-02 17:44:01 +0100

    remove stale .lock files

--- .azure-pipelines/scripts/checkout_submodules.sh
+++ .azure-pipelines/scripts/checkout_submodules.sh
@@ -5,6 +5,7 @@ set -ex
 mkdir -p ~/.ssh > /dev/null
 chmod 700 ~/.ssh
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+find .git -type f -name '*.lock' -exec rm -v {} +
 git reset --hard
 git submodule sync
 git submodule update --init --force --recursive
