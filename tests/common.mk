66f79498c499090883a53e3992f2123531d2139e
Maik Riechert <marieche@microsoft.com>
2020-05-04 10:53:02 +0100

    expose sgx-lkl-docker tool to tests

--- tests/common.mk
+++ tests/common.mk
@@ -5,11 +5,13 @@ export SGXLKL_ROOT=$(realpath $(mkfile_dir)..)
 ifeq (${SGXLKL_PREFIX},)
 	export SGXLKL_STARTER=$(SGXLKL_ROOT)/build/sgx-lkl-run-oe
 	export SGXLKL_DISK_TOOL=${SGXLKL_ROOT}/tools/sgx-lkl-disk
+	export SGXLKL_DOCKER_TOOL=${SGXLKL_ROOT}/tools/sgx-lkl-docker
 	export SGXLKL_GDB=${SGXLKL_ROOT}/tools/gdb/sgx-lkl-gdb
 	export SGXLKL_JAVA_RUN=${SGXLKL_ROOT}/tools/sgx-lkl-java
 else
 	export SGXLKL_STARTER=$(SGXLKL_PREFIX)/bin/sgx-lkl-run-oe
 	export SGXLKL_DISK_TOOL=${SGXLKL_PREFIX}/bin/sgx-lkl-disk
+	export SGXLKL_DOCKER_TOOL=${SGXLKL_PREFIX}/bin/sgx-lkl-docker
 	export SGXLKL_GDB=${SGXLKL_PREFIX}/bin/sgx-lkl-gdb
 	export SGXLKL_JAVA_RUN=${SGXLKL_PREFIX}/bin/sgx-lkl-java
 endif
